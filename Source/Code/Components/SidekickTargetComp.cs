dd8afb62bf2c6e7e6f12ddf1e1f611be9af4dc2b
David Higueros <davhig22@byu.edu>
2024-09-13 17:44:21 -0600

    Started work on Sidekick Target

---
+++ Source/Code/Components/SidekickTargetComp.cs
@@ -0,0 +1,25 @@
+﻿using Monocle;
+using Microsoft.Xna.Framework;
+using Celeste.Mod.BossesHelper.Code.Entities;
+
+namespace Celeste.Mod.BossesHelper.Code.Components
+{
+    [Tracked]
+    internal class SidekickTargetComp : Component
+    {
+        public SidekickTarget target;
+
+        public Vector2 offset;
+
+        public SidekickTargetComp(Vector2 position, Vector2 offset, float radius) : base(active: true, visible: false)
+        {
+            target = new SidekickTarget(position + offset, radius);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            target.Position = Entity.Position + offset;
+        }
+    }
+}
---
+++ Source/Code/Entities/SidekickTarget.cs
@@ -0,0 +1,26 @@
+﻿using Monocle;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.BossesHelper.Code.Entities
+{
+    internal class SidekickTarget : Entity
+    {
+        public Circle circle;
+
+        public SidekickTarget(Vector2 position, float radius) : base(position)
+        {
+            circle = new Circle(radius, position.X, position.Y);
+        }
+
+        public override void Added(Scene scene)
+        {
+            base.Added(scene);
+            Depth = -1000;
+        }
+    }
+}
