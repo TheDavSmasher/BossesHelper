6925a8002296939a2d77a837bdee81cdd7217366
David Higueros <davhig22@byu.edu>
2024-09-13 19:34:53 -0600

    Removed unused using statements

--- Source/Code/Entities/BadelineSidekick.cs
+++ Source/Code/Entities/BadelineSidekick.cs
@@ -1,11 +1,5 @@
 ﻿using Monocle;
 using Microsoft.Xna.Framework;
-using Celeste;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Celeste.Mod.BossesHelper.Code.Entities
 {
