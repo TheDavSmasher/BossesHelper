bb5e4aff38e8f9f750a542df8756c0a5c0ad37b5
David Higueros <davhig22@byu.edu>
2024-09-15 16:04:25 -0600

    Base constructor refactor

--- Source/Code/Components/SidekickTargetComp.cs
+++ Source/Code/Components/SidekickTargetComp.cs
@@ -10,7 +10,8 @@ namespace Celeste.Mod.BossesHelper.Code.Components
 
         public Vector2 Offset {  get; set; }
 
-        public SidekickTargetComp(string bossName, Vector2 position, Vector2 offset, float radius = 4f) : base(active: true, visible: false)
+        public SidekickTargetComp(string bossName, Vector2 position, Vector2 offset, float radius = 4f)
+            : base(active: true, visible: false)
         {
             target = new SidekickTarget(bossName, position + offset, radius);
             Offset = offset;
--- Source/Code/Entities/SidekickTarget.cs
+++ Source/Code/Entities/SidekickTarget.cs
@@ -10,7 +10,8 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
 
         public readonly string BossName;
 
-        public SidekickTarget(string bossName, Vector2 position, float radius = 4f) : base(position)
+        public SidekickTarget(string bossName, Vector2 position, float radius = 4f)
+            : base(position)
         {
             circle = new Circle(radius, position.X, position.Y);
             BossName = bossName;
