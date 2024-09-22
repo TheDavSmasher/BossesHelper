0316f76bd71d74535d702dbb8d1165aa1506371f
David Higueros <davhig22@byu.edu>
2024-09-21 18:21:49 -0600

    Added basic entity with sprite and hitbox for ease of attack making

---
+++ Source/Code/Entities/AttackEntity.cs
@@ -0,0 +1,40 @@
+﻿using NLua;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.BossesHelper.Code.Entities
+{
+    internal class AttackEntity : Entity
+    {
+        private Sprite sprite;
+
+        private LuaFunction onCollide;
+
+        public AttackEntity(Vector2 position, Collider attackbox, LuaFunction onPlayer, string spriteName, float xScale = 1f, float yScale = 1f) : base(position)
+        {
+            base.Collider = attackbox;
+            sprite = GFX.SpriteBank.Create(spriteName);
+            sprite.Scale = new Vector2(xScale, yScale);
+            Add(sprite);
+            onCollide = onPlayer;
+            Add(new PlayerCollider(OnPlayer));
+        }
+
+        public void PlayAnim(string anim)
+        {
+            if (sprite.Has(anim))
+            {
+                sprite.Play(anim);
+            }
+            else
+            {
+                Logger.Log(LogLevel.Warn, "BossesHelper/AttackEntity", "Animation specified does not exist!");
+            }
+        }
+
+        private void OnPlayer(Player player)
+        {
+            onCollide.Call(player);
+        }
+    }
+}
