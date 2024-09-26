488558be4a68cc91af7fde3d13de201841bcc10e
David Higueros <davhig22@byu.edu>
2024-09-26 14:03:05 -0600

    Added EntityChainComp

---
+++ Source/Code/Components/EntityChainComp.cs
@@ -0,0 +1,31 @@
+﻿using Monocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.BossesHelper.Code.Components
+{
+    internal class EntityChainComp : Component
+    {
+        public Entity chained;
+
+        public bool chainedPosition;
+
+        public EntityChainComp(Entity entity, bool chainPosition, bool active, bool visible) : base(active, visible)
+        {
+            this.chained = entity;
+            this.chainedPosition = chainPosition;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (chainedPosition)
+            {
+                chained.Position = Entity.Position + chained.Position;
+            }
+        }
+    }
+}
