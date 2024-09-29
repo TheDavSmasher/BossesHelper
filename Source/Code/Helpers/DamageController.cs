cd24bcc9784168abc173e50137519d0fced14034
David Higueros <davhig22@byu.edu>
2024-09-28 23:21:32 -0600

    Added HealthBar and Damage Controller

---
+++ Source/Code/Helpers/DamageController.cs
@@ -0,0 +1,130 @@
+﻿using Monocle;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections;
+
+namespace Celeste.Mod.BossesHelper.Code.Helpers
+{
+    [Tracked(false)]
+    public class DamageController : Entity
+    {
+        private float damageCooldown;
+
+        public int health;
+
+        public DamageController()
+        {
+            AddTag(Tags.Global);
+            health = BossesHelperModule.playerHealthBar.health;
+        }
+
+        public void TakeDamage(Vector2 origin, int amount = 1)
+        {
+            if (damageCooldown > 0 || SaveData.Instance.Assists.Invincible)
+            {
+                return;
+            }
+            Level level = SceneAs<Level>();
+            if (level.InCutscene)
+            {
+                return;
+            }
+            damageCooldown = 1f;
+            health -= amount;
+            level.Shake();
+            Input.Rumble(RumbleStrength.Strong, RumbleLength.Long);
+            level.Flash(Color.Red * 0.3f);
+            Player entity = Engine.Scene.Tracker.GetEntity<Player>();
+            if (entity == null)
+            {
+                return;
+            }
+            if (health > 0)
+            {
+                Audio.Play("event:/char/madeline/predeath");
+                Add(new Coroutine(PlayerStagger(entity, origin)));
+                Add(new Coroutine(PlayerInvincible(entity)));
+            }
+            else
+            {
+                entity.Die((entity.Center - origin).SafeNormalize());
+            }
+            if (BossesHelperModule.playerHealthBar != null)
+            {
+                for (int i = 0; i < amount; i++)
+                {
+                    BossesHelperModule.playerHealthBar.DecreaseHealth();
+                }
+            }
+            else
+            {
+                Logger.Log("Bosses Helper", "No Health Bar has been initialized");
+            }
+        }
+
+        public void RecoverHealth(int amount = 1)
+        {
+
+        }
+
+        private IEnumerator PlayerStagger(Player player, Vector2 bounce)
+        {
+            if (bounce != Vector2.Zero)
+            {
+                Celeste.Freeze(0.05f);
+                yield return null;
+                Vector2 from = player.Position;
+                Vector2 to = new Vector2(from.X + (float)((!(bounce.X < 0f)) ? 1 : (-1)) * 20f, from.Y - 5f);
+                Tween tween = Tween.Create(Tween.TweenMode.Oneshot, Ease.CubeOut, 0.2f, start: true);
+                Add(tween);
+                tween.OnUpdate = delegate (Tween t)
+                {
+                    Vector2 val = from + (to - from) * t.Eased;
+                    player.MoveToX(val.X);
+                    player.MoveToY(val.Y);
+                    player.Sprite.Rotation = (float)(Math.Floor(t.Eased * 4f) * 6.2831854820251465);
+                };
+                yield return tween.Duration;
+                tween.Stop();
+            }
+        }
+
+        private IEnumerator PlayerInvincible(Player player)
+        {
+            int times = 2;
+            Tween tween = Tween.Create(Tween.TweenMode.Oneshot, Ease.CubeOut, damageCooldown, start: true);
+            Add(tween);
+            tween.OnUpdate = delegate
+            {
+                if (base.Scene.OnInterval(0.02f))
+                {
+                    if (times <= 0)
+                    {
+                        player.Sprite.Visible = false;
+                        player.Hair.Visible = false;
+                        times = 2;
+                    }
+                    else
+                    {
+                        player.Sprite.Visible = true;
+                        player.Hair.Visible = true;
+                        times--;
+                    }
+                }
+            };
+            yield return tween.Duration;
+            tween.Stop();
+            player.Sprite.Visible = true;
+            player.Hair.Visible = true;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (damageCooldown > 0)
+            {
+                damageCooldown -= Engine.DeltaTime;
+            }
+        }
+    }
+}
---
+++ Source/Code/Helpers/DamageHealthBar.cs
@@ -0,0 +1,147 @@
+﻿using Monocle;
+using Microsoft.Xna.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Celeste.Mod.BossesHelper.Code.Helpers
+{
+    [Tracked(false)]
+    public class DamageHealthBar : Entity
+    {
+        private class HealthIcon : Entity
+        {
+            private readonly  Sprite icon;
+
+            private readonly string createAnim;
+
+            private readonly string removeAnim;
+
+            public HealthIcon(string spriteName, string createAnim, string removeAnim)
+            {
+                Add(icon = GFX.SpriteBank.Create(spriteName));
+                base.Tag = Tags.HUD;
+                AddTag(Tags.Global);
+                this.createAnim = createAnim;
+                this.removeAnim = removeAnim;
+            }
+
+            public void DrawIcon(Vector2 position)
+            {
+                Position = position;
+                Add(new Coroutine(DrawRoutine()));
+            }
+
+            private IEnumerator DrawRoutine()
+            {
+                if (!string.IsNullOrEmpty(createAnim) && icon.Has(createAnim))
+                    icon.Play(createAnim);
+                yield return 0.32f;
+            }
+
+            public void RemoveIcon()
+            {
+                Add(new Coroutine(RemoveRoutine()));
+            }
+
+            private IEnumerator RemoveRoutine()
+            {
+                if (!string.IsNullOrEmpty(removeAnim) && icon.Has(removeAnim))
+                    icon.Play(removeAnim);
+                yield return 0.88f;
+                RemoveSelf();
+            }
+
+            public override void Render()
+            {
+                base.Render();
+                icon.Visible = !base.Scene.Paused;
+            }
+        }
+
+        private Level level;
+
+        private readonly List<HealthIcon> healthIcons;
+
+        private readonly string spritePath;
+
+        private readonly string spriteCreate;
+
+        private readonly string spriteRemove;
+
+        private readonly float iconSpace;
+
+        public int health;
+
+        public DamageHealthBar(int health, string iconSprite, string createIconAnim, string removeIconAnim, Vector2 pos, float iconSeparation)
+            : base(pos)
+        {
+            healthIcons = new List<HealthIcon>();
+            spritePath = iconSprite;
+            spriteCreate = createIconAnim;
+            spriteRemove = removeIconAnim;
+            for (int i = 0; i < health; i++)
+            {
+                healthIcons.Add(new HealthIcon(spritePath, spriteCreate, spriteRemove));
+            }
+            this.health = health;
+            iconSpace = iconSeparation;
+            base.Tag = Tags.HUD;
+            AddTag(Tags.Global);
+        }
+
+        public override void Awake(Scene scene)
+        {
+            base.Awake(scene);
+            level = SceneAs<Level>();
+            DrawHealthBar();
+        }
+
+        private void DrawHealthBar()
+        {
+            for (int i = 0; i < health; i++)
+            {
+                level.Add(healthIcons[i]);
+                healthIcons[i].DrawIcon(Position + Vector2.UnitX * iconSpace * i);
+            }
+        }
+
+        public void RefillHealth()
+        {
+            for(int i = 0; i < health; i++)
+            {
+                HealthIcon healthIcon = new HealthIcon(spritePath, spriteCreate, spriteRemove);
+                healthIcons.Add(healthIcon);
+                level.Add(healthIcon);
+                healthIcon.DrawIcon(Position + Vector2.UnitX * iconSpace * i);
+            }
+        }
+
+        public void IncreaseHealth()
+        {
+            HealthIcon healthIcon = new HealthIcon(spritePath, spriteCreate, spriteRemove);
+            healthIcons.Add(healthIcon);
+            level.Add(healthIcon);
+            healthIcon.DrawIcon(Position + Vector2.UnitX * iconSpace * (healthIcons.Count - 1));
+        }
+
+        public void DecreaseHealth()
+        {
+            if (healthIcons.Count > 0)
+            {
+                healthIcons[healthIcons.Count - 1].RemoveIcon();
+                healthIcons.RemoveAt(healthIcons.Count - 1);
+            }
+            else
+            {
+                Logger.Log("Health Render", "No Health Icon to remove");
+            }
+        }
+
+        public override void Removed(Scene scene)
+        {
+            base.Removed(scene);
+            healthIcons.ForEach(x => x.RemoveSelf());
+            healthIcons.Clear();
+        }
+    }
+}
