b7097dec419399a0210cc2500694bfad7ff91029
David Higueros <davhig22@byu.edu>
2024-09-29 13:37:45 -0600

    Added overhead controller which the user can add

---
+++ Source/Code/Entities/HealthSystemController.cs
@@ -0,0 +1,25 @@
+﻿using Celeste.Mod.Entities;
+using Monocle;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.BossesHelper.Code.Entities
+{
+    [Tracked(false)]
+    [CustomEntity("BossesHelper/HealthSystemController")]
+    public class HealthSystemController : Entity
+    {
+        public HealthSystemController(EntityData data, Vector2 offset)
+        {
+            BossesHelperModule.healthData.healthBarPos = data.Position + offset;
+            BossesHelperModule.healthData.iconSprite = data.Attr("healthIcon");
+            BossesHelperModule.healthData.startAnim = data.Attr("healthIconCreateAnim");
+            BossesHelperModule.healthData.endAnim = data.Attr("healthIconRemoveAnim");
+            BossesHelperModule.healthData.iconSeparation = data.Float("healthIconSeparation");
+            BossesHelperModule.healthData.globalController = data.Bool("isGlobal");
+            BossesHelperModule.healthData.playerHealthVal = data.Int("playerHealth");
+            BossesHelperModule.healthData.damageCooldown = data.Float("damageCooldown", 1f);
+            BossesHelperModule.playerHealthBar ??= new();
+            BossesHelperModule.playerDamageController ??= new();
+        }
+    }
+}
