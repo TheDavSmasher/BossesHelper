34c5f14f75fbbb0804d6e1aa7b78b6c61014fe9d
David Higueros <davhig22@byu.edu>
2024-09-29 20:39:56 -0600

    Added functionality and minor activation logic to HealthController

--- Source/Code/Entities/HealthSystemController.cs
+++ Source/Code/Entities/HealthSystemController.cs
@@ -8,19 +8,61 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
     [CustomEntity("BossesHelper/HealthSystemController")]
     public class HealthSystemController : Entity
     {
+        public bool enabled;
+
+        private readonly string activateFlag;
+
         public HealthSystemController(EntityData data, Vector2 offset)
         {
-            BossesHelperModule.healthData.healthBarPos = data.Position + offset;
-            BossesHelperModule.healthData.iconSprite = data.Attr("healthIcon");
-            BossesHelperModule.healthData.startAnim = data.Attr("healthIconCreateAnim");
-            BossesHelperModule.healthData.endAnim = data.Attr("healthIconRemoveAnim");
-            BossesHelperModule.healthData.iconSeparation = data.Float("healthIconSeparation");
-            BossesHelperModule.healthData.globalController = data.Bool("isGlobal");
-            BossesHelperModule.healthData.playerHealthVal = data.Int("playerHealth");
-            BossesHelperModule.healthData.damageCooldown = data.Float("damageCooldown", 1f);
-            BossesHelperModule.healthSystemController ??= this;
             BossesHelperModule.playerHealthBar ??= new();
             BossesHelperModule.playerDamageController ??= new();
+            if (BossesHelperModule.healthSystemController == null)
+            {
+                BossesHelperModule.healthSystemController = this;
+                BossesHelperModule.healthData.healthBarPos = data.Position + offset;
+                BossesHelperModule.healthData.iconSprite = data.Attr("healthIcon");
+                BossesHelperModule.healthData.startAnim = data.Attr("healthIconCreateAnim");
+                BossesHelperModule.healthData.endAnim = data.Attr("healthIconRemoveAnim");
+                BossesHelperModule.healthData.iconSeparation = data.Float("healthIconSeparation");
+                BossesHelperModule.healthData.globalController = data.Bool("isGlobal");
+                BossesHelperModule.healthData.playerHealthVal = data.Int("playerHealth");
+                BossesHelperModule.healthData.damageCooldown = data.Float("damageCooldown", 1f);
+                BossesHelperModule.healthData.applySystemInstantly = data.Bool("applySystemInstantly");
+            }
+            activateFlag = data.Attr("activationFlag");
+            enabled = false;
+        }
+
+        public override void Added(Scene scene)
+        {
+            base.Added(scene);
+            if (BossesHelperModule.healthData.applySystemInstantly || string.IsNullOrEmpty(activateFlag))
+                EnableHealthSystem();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (SceneAs<Level>().Session.GetFlag(activateFlag))
+                EnableHealthSystem();
+        }
+
+        public override void Removed(Scene scene)
+        {
+            base.Removed(scene);
+            BossesHelperModule.playerHealthBar.RemoveSelf();
+            BossesHelperModule.playerDamageController.RemoveSelf();
+        }
+
+        public void EnableHealthSystem()
+        {
+            enabled = true;
+            Level level = SceneAs<Level>();
+            if (level != null)
+            {
+                level.Add(BossesHelperModule.playerHealthBar);
+                level.Add(BossesHelperModule.playerDamageController);
+            }
         }
     }
 }
