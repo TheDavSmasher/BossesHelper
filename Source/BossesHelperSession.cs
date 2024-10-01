bfbf4ea5c699f11a5e66d0e4f0652d24171588a9
David Higueros <davhig22@byu.edu>
2024-10-01 12:16:25 -0600

    Added HealthSystem data and entities to Session instead of Module

--- Source/BossesHelperSession.cs
+++ Source/BossesHelperSession.cs
@@ -1,3 +1,7 @@
+using Microsoft.Xna.Framework;
+using Celeste.Mod.BossesHelper.Code.Entities;
+using Celeste.Mod.BossesHelper.Code.Helpers;
+
 namespace Celeste.Mod.BossesHelper;
 
 public class BossesHelperSession : EverestModuleSession
@@ -12,4 +16,41 @@ public class BossesHelperSession : EverestModuleSession
     }
 
     public BossPhase BossPhaseSaved { get; set; }
+
+    public struct HealthSystemData
+    {
+        public EntityData entityDataUsed;
+
+        public int playerHealthVal;
+
+        public string iconSprite;
+
+        public string startAnim;
+
+        public string endAnim;
+
+        public float iconSeparation;
+
+        public Vector2 healthBarPos;
+
+        public Vector2 healthIconScale;
+
+        public float damageCooldown;
+
+        public bool globalController;
+
+        public bool globalHealth;
+
+        public bool applySystemInstantly;
+
+        public HealthSystemManager.CrushEffect playerOnCrush;
+    }
+
+    public HealthSystemData healthData;
+
+    public HealthSystemManager mapHealthSystemManager { get; set; }
+
+    public DamageController mapDamageController { get; set; }
+
+    public DamageHealthBar mapHealthBar { get; set; }
 }
