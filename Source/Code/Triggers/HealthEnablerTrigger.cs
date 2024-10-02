77050e35642a1c40c2de0c06f634e3b58f4085ec
David Higueros <davhig22@byu.edu>
2024-10-02 10:46:40 -0600

    Added HealthEnableTrigger

---
+++ Source/Code/Triggers/HealthEnablerTrigger.cs
@@ -0,0 +1,37 @@
+﻿using Celeste.Mod.BossesHelper.Code.Entities;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.BossesHelper.Code.Triggers
+{
+    [CustomEntity("BossesHelper/HealthEnableTrigger")]
+    public class HealthEnablerTrigger : Trigger
+    {
+        public bool enableState;
+
+        public HealthEnablerTrigger(EntityData data, Vector2 offset)
+            : base(data, offset)
+        {
+            enableState = data.Bool("enableState");
+        }
+
+        public override void OnEnter(Player player)
+        {
+            base.OnEnter(player);
+            if (enableState)
+            {
+                SceneAs<Level>().Tracker.GetEntity<HealthSystemManager>().EnableHealthSystem();
+            }
+            else
+            {
+                SceneAs<Level>().Tracker.GetEntity<HealthSystemManager>().DisableHealthSystem();
+            }
+        }
+    }
+}
