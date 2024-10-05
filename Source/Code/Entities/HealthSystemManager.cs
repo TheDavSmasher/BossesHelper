236c8f7050afc2e0fe8d7fca12584899e5348cc8
David Higueros <davhig22@byu.edu>
2024-10-05 12:20:00 -0600

    Removed EntityID

--- Source/Code/Entities/HealthSystemManager.cs
+++ Source/Code/Entities/HealthSystemManager.cs
@@ -8,8 +8,6 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
     [CustomEntity("BossesHelper/HealthSystemManager")]
     public class HealthSystemManager : Entity
     {
-        private readonly EntityID id;
-
         public enum CrushEffect
         {
             PushOut,
@@ -23,7 +21,7 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
 
         private readonly string activateFlag;
 
-        public HealthSystemManager(EntityData data, Vector2 _, EntityID id)
+        public HealthSystemManager(EntityData data, Vector2 _)
         {
             BossesHelperModule.Session.mapHealthSystemManager ??= this;
             BossesHelperModule.Session.healthData.iconSprite = data.Attr("healthIcon", "bird");
@@ -41,7 +39,6 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
             BossesHelperModule.Session.healthData.playerOnCrush = data.Enum<CrushEffect>("crushEffect");
             activateInstantly = data.Bool("applySystemInstantly");
             activateFlag = data.Attr("activationFlag");
-            this.id = id;
             enabled = false;
             if (BossesHelperModule.Session.healthData.globalController)
                 AddTag(Tags.Global);
