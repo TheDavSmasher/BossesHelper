4aa5ffb832c834b6bbd2562851d2ea7e0f1f12f9
David Higueros <davhig22@byu.edu>
2024-10-05 12:53:54 -0600

    Added AddHealthSystemTrigger, loenn plugin, and tooltips

---
+++ Loenn/entities/addHealthSystemTrigger.lua
@@ -0,0 +1,61 @@
+local addHealthSystemTrigger = {}
+
+local crushModeOpts = {
+    {"Push Out", "pushOut"},
+    {"Solid On Invincible Player", "invincibleSolid"},
+    {"Instant Death", "instantDeath"}
+}
+
+addHealthSystemTrigger.name = "BossesHelper/HealthSystemManager"
+addHealthSystemTrigger.depth = 0
+addHealthSystemTrigger.nodeLimits = {0, 0}
+addHealthSystemTrigger.fieldInformation = {
+    healthIconSeparation = {
+        fieldType = "number"
+    },
+    playerHealth = {
+        fieldType = "integer",
+        minimumValue = 1
+    },
+    damageCooldown = {
+        fieldType = "number",
+        minimumValue = 0
+    },
+    healthIconScreenX = {
+        fieldType = "number",
+        minimumValue = 0,
+        maximumValue = 1920
+    },
+    healthIconScreenY = {
+        fieldType = "number",
+        minimumValue = 0,
+        maximumValue = 1080
+    },
+    crushEffect = {
+        options = crushModeOpts,
+        editable = false
+    }
+}
+addHealthSystemTrigger.placements = {
+    name = "Add Health System Trigger",
+    data = {
+        activationFlag = "",
+        healthIcon = "",
+        healthIconCreateAnim = "",
+        healthIconRemoveAnim = "",
+        healthIconScreenX = "160",
+        healthIconScreenY = "950",
+        healthIconScaleX = "1",
+        healthIconScaleY = "1",
+        healthIconSeparation = "1.0",
+        playerHealth = "3",
+        damageCooldown = "1",
+        crushEffect = "instantDeath",
+        isGlobal = true,
+        globalHealth = false,
+        applySystemInstantly = true,
+        onlyOnce = true
+    }
+}
+
+return addHealthSystemTrigger
---
+++ Source/Code/Triggers/AddHealthSystemTrigger.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Celeste.Mod.BossesHelper.Code.Entities;
+using Celeste.Mod.Entities;
+
+namespace Celeste.Mod.BossesHelper.Code.Triggers
+{
+    [CustomEntity("BossesHelper/AddHealthSystemTrigger")]
+    public class AddHealthSystemTrigger : Trigger
+    {
+        private EntityID id;
+
+        private readonly bool onlyOnce;
+
+        public AddHealthSystemTrigger(EntityData data, Vector2 offset, EntityID id)
+            : base(data, offset)
+        {
+            this.id = id;
+            onlyOnce = data.Bool("onlyOnce", true);
+            new HealthSystemManager(data, offset);
+        }
+
+        public override void OnEnter(Player player)
+        {
+            base.OnEnter(player);
+            SceneAs<Level>().Add(BossesHelperModule.Session.mapHealthSystemManager);
+            if (onlyOnce)
+            {
+                SceneAs<Level>().Session.DoNotLoad.Add(id);
+            }
+            RemoveSelf();
+        }
+    }
+}
