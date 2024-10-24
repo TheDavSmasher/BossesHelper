be02637ad8b62dfad3c7b66ba847c062424528b1
David Higueros <davhig22@byu.edu>
2024-10-24 16:14:15 -0600

    Minor refactor

--- Source/Code/Entities/AttackActor.cs
+++ Source/Code/Entities/AttackActor.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Monocle;
 using NLua;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Celeste.Mod.BossesHelper.Code.Entities
 {
