7a4a7e4cc8f299cd697d2352dddcf979c2a2bac8
David Higueros <davhig22@byu.edu>
2024-10-26 01:02:52 -0600

    Added empty Exports file for ModInterop API usage later

---
+++ Source/BossesHelperExports.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Monocle;
+using MonoMod.ModInterop;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.BossesHelper
+{
+    [ModExportName("BossesHelper")]
+    public static class BossesHelperExports
+    {
+        //Nothing for now
+    }
+}
