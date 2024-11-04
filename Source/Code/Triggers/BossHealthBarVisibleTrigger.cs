ce11ad55fb7faa4fb2a269fc33d943d18a20bb5b
David Higueros <davhig22@byu.edu>
2024-11-03 19:34:47 -0700

    Added BossHealthBarVisibleTrigger and fixed name for Player version of trigger

---
+++ Source/Code/Triggers/BossHealthBarVisibleTrigger.cs
@@ -0,0 +1,38 @@
+﻿using Celeste.Mod.BossesHelper.Code.Entities;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.BossesHelper.Code.Triggers
+{
+    [CustomEntity("BossesHelper/BossHealthBarVisibleTrigger")]
+    public class BossHealthBarVisibleTrigger : Trigger
+    {
+        private readonly bool state;
+
+        private readonly bool onlyOnce;
+
+        private readonly Vector2 node;
+
+        public BossHealthBarVisibleTrigger(EntityData data, Vector2 offset)
+            : base(data, offset)
+        {
+            state = data.Bool("visible");
+            onlyOnce = data.Bool("onlyOnce");
+            node = data.Nodes[0];
+        }
+
+        public override void OnEnter(Player player)
+        {
+            base.OnEnter(player);
+            BossHealthBar bar = SceneAs<Level>().Tracker.GetNearestEntity<BossHealthBar>(node);
+            if (bar != null)
+            {
+                bar.Visible = state;
+                if (onlyOnce)
+                {
+                    RemoveSelf();
+                }
+            }
+        }
+    }
+}
