92d10bb9e8debe3e92cd2879b9334115adc64433
David Higueros <davhig22@byu.edu>
2024-11-12 22:03:49 -0700

    Removed unused statement

--- Source/Code/Helpers/BossesHelperUtils.cs
+++ Source/Code/Helpers/BossesHelperUtils.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Monocle;
 using Microsoft.Xna.Framework;
-using System.Runtime.InteropServices;
 
 namespace Celeste.Mod.BossesHelper.Code.Helpers
 {
