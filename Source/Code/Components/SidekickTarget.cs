6161868a9f9dc748ae20efeaea3eb1b178f253ce
David Higueros <davhig22@byu.edu>
2024-11-21 00:27:23 -0700

    Updated to include Collision Logic within component

--- Source/Code/Components/SidekickTarget.cs
+++ Source/Code/Components/SidekickTarget.cs
@@ -1,57 +1,54 @@
 ﻿using Monocle;
 using Microsoft.Xna.Framework;
-using Celeste.Mod.BossesHelper.Code.Entities;
 using System;
 
 namespace Celeste.Mod.BossesHelper.Code.Components
 {
-    public class SidekickTarget(Action onLaser, string bossId, Vector2 position, Collider target) : Component(active: true, visible: false)
+    [Tracked(false)]
+    public class SidekickTarget(Action onLaser, string bossId, Collider target) : Component(active: true, visible: true)
     {
-        private readonly SidekickTargetCollider sidekickTarget = new(position, onLaser, target);
-
         public readonly string BossID = bossId;
 
-        public Collider Collider
+        private readonly Action onLaser = onLaser;
+
+        public Collider Collider = target;
+
+        public bool CollideCheck(Vector2 from, Vector2 to)
         {
-            get
+            if (Entity == null) return false;
+            if (Entity.Collidable)
             {
-                return sidekickTarget.Collider;
+                Collider original = base.Entity.Collider;
+                base.Entity.Collider = Collider;
+                bool hit = Entity.CollideLine(from, to);
+                base.Entity.Collider = original;
+                return hit;
             }
-            set
-            {
-                sidekickTarget.Collider = value;
-            }
+            return false;
         }
 
-        public Vector2 Position
+        public void OnLaser()
         {
-            get
+            onLaser?.Invoke();
+        }
+
+        public override void DebugRender(Camera camera)
+        {
+            if (Collider != null)
             {
-                return sidekickTarget.Position;
+                if (Collider is Circle single)
+                {
+                    Draw.Circle(single.AbsolutePosition, single.Radius, Color.AliceBlue, 10);
+                }
+                if (Collider is ColliderList colliderList)
+                {
+                    foreach (Collider collider in colliderList.colliders)
+                    {
+                        Circle target = collider as Circle;
+                        Draw.Circle(target.AbsolutePosition, target.Radius, Color.AliceBlue, 10);
+                    }
+                }
             }
-            set
-            {
-                sidekickTarget.Position = value;
-            }
-        }
-
-        public override void EntityAdded(Scene scene)
-        {
-            base.EntityAdded(scene);
-            (scene as Level).Add(sidekickTarget);
-            sidekickTarget.Depth = -1000;
-        }
-
-        public override void Removed(Entity entity)
-        {
-            base.Removed(entity);
-            sidekickTarget.RemoveSelf();
-        }
-
-        public override void Update()
-        {
-            base.Update();
-            Position = Entity.Position;
         }
     }
 }
