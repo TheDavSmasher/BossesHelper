cd6c6ceef3d615fdc28a9170791a2d776967f381
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2024-12-19 15:24:19 -0700

    Created SavePointTracker Component

---
+++ Source/Code/Components/SavePointTracker.cs
@@ -0,0 +1,11 @@
+﻿using Celeste.Mod.BossesHelper.Code.Entities;
+using Monocle;
+
+namespace Celeste.Mod.BossesHelper.Code.Components
+{
+    [Tracked(false)]
+    public class SavePointTracker(GlobalSavePoint savePoint) : Component(active: true, visible: false)
+    {
+        public GlobalSavePoint savePoint = savePoint;
+    }
+}
