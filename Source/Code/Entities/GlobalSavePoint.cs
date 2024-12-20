66f42d4436aac65ea5e0c1ebe8d791296420a593
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2024-12-19 20:18:42 -0700

    Added Tracked and CustomEntity Attributes

--- Source/Code/Entities/GlobalSavePoint.cs
+++ Source/Code/Entities/GlobalSavePoint.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
 using Monocle;
 
 namespace Celeste.Mod.BossesHelper.Code.Entities
 {
+    [Tracked(false)]
+    [CustomEntity("BossesHelper/PlayerSavePoint")]
     public class GlobalSavePoint : Entity
     {
         public EntityID ID;
