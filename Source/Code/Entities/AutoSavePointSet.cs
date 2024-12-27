8856cf5ca8d19b406be251a525c8846c3818f774
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2024-12-26 22:41:27 -0700

    Created AutoSavePointSet to set a Save Point as soon as the player enters the Level

---
+++ Source/Code/Entities/AutoSavePointSet.cs
@@ -0,0 +1,41 @@
+﻿using Celeste.Mod.BossesHelper.Code.Components;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.BossesHelper.Code.Entities
+{
+    [CustomEntity("BossesHelper/AutoSavePointSet ")]
+    public class AutoSavePointSet : Entity
+    {
+        private readonly EntityID ID;
+
+        private readonly Player.IntroTypes spawnType;
+
+        private GlobalSavePointChanger Changer;
+
+        public AutoSavePointSet(EntityData data, Vector2 _, EntityID id)
+        {
+            ID = id;
+            spawnType = data.Enum<Player.IntroTypes>("spawnType");
+        }
+
+        public override void Awake(Scene scene)
+        {
+            base.Awake(scene);
+            Session session = (scene as Level).Session;
+            if (session.RespawnPoint is Vector2 spawn)
+            {
+                Add(Changer = new(ID.Level, spawn, spawnType));
+                Changer.Update();
+            }
+            RemoveSelf();
+        }
+
+        public override void Removed(Scene scene)
+        {
+            base.Removed(scene);
+            (scene as Level).Session.DoNotLoad.Add(ID);
+        }
+    }
+}
