43c7149a736049764ec11024dba0ed17d93a06a6
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2024-12-27 19:13:45 -0700

    Used offset for correct node and changed order of code in Added and Awake

--- Source/Code/Entities/AutoSavePointSet.cs
+++ Source/Code/Entities/AutoSavePointSet.cs
@@ -18,20 +18,21 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
 
         private GlobalSavePointChanger Changer;
 
-        public AutoSavePointSet(EntityData data, Vector2 _, EntityID id)
+        public AutoSavePointSet(EntityData data, Vector2 offset, EntityID id)
+            : base()
         {
             ID = id;
             spawnType = data.Enum<Player.IntroTypes>("respawnType");
             if (data.FirstNodeNullable() is Vector2 spawn)
             {
-                spawnPosition = spawn;
+                spawnPosition = spawn + offset;
             }
             onlyOnce = data.Bool("onlyOnce");
         }
 
-        public override void Awake(Scene scene)
+        public override void Added(Scene scene)
         {
-            base.Awake(scene);
+            base.Added(scene);
             Session session = (scene as Level).Session;
             if (spawnPosition is Vector2 node)
             {
@@ -41,6 +42,11 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
             {
                 Add(Changer = new(ID, spawn, spawnType));
             }
+        }
+
+        public override void Awake(Scene scene)
+        {
+            base.Awake(scene);
             Changer?.Update();
             RemoveSelf();
         }
