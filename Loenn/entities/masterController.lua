1e0cad308910563d047fd519e3bbe80935640dfc
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-01-04 14:46:54 -0700

    Created second placement for Sidekick mode iof Boss controller

--- Loenn/entities/masterController.lua
+++ Loenn/entities/masterController.lua
@@ -11,7 +11,6 @@ local hurtModeOpts = {
 bossMasterController.name = "BossesHelper/BossController"
 bossMasterController.depth = 0
 bossMasterController.texture = "loenn/BossesHelper/BossController"
---bossMasterController.justification = {0.5, 1.0}
 --bossMasterController.nodeLineRenderType = "line"
 bossMasterController.nodeLimits = {0, 0}
 bossMasterController.fieldInformation = {
@@ -19,6 +18,9 @@ bossMasterController.fieldInformation = {
         fieldType = "integer"
     },
     __Boss_pad = {
+        fieldType = "spacer"
+    },
+    __Boss_pad_ = {
         fieldType = "spacer"
     },
     hurtMode = {
@@ -65,40 +67,67 @@ bossMasterController.fieldInformation = {
     }
 }
 
-bossMasterController.fieldOrder = {
-    "x", "y", "bossID", "bossSprite", "bossHealthMax", "hurtMode", "maxFall", "bossHitCooldown", "baseGravityMultiplier", "sidekickCooldown",
-    "hitboxMetadataPath", "patternsPath", "attacksPath", "eventsPath", "functionsPath", "__Boss_pad",
-    "dynamicFacing", "mirrorSprite", "killOnContact", "startAttackingImmediately", "sidekickFreeze"
-}
+function bossMasterController.fieldOrder(entity)
+    if entity.hurtMode == "sidekickAttack" then
+        return {
+            "x", "y", "bossID", "bossSprite", "bossHealthMax", "hurtMode", "maxFall", "bossHitCooldown", "baseGravityMultiplier", "sidekickCooldown",
+            "hitboxMetadataPath", "patternsPath", "attacksPath", "eventsPath", "functionsPath", "__Boss_pad",
+            "dynamicFacing", "mirrorSprite", "killOnContact", "startAttackingImmediately", "sidekickFreeze"
+        }
+    else
+        return {
+            "x", "y", "bossID", "bossSprite", "bossHealthMax", "hurtMode", "maxFall", "bossHitCooldown", "baseGravityMultiplier", "__Boss_pad",
+            "hitboxMetadataPath", "patternsPath", "attacksPath", "eventsPath", "functionsPath", "__Boss_pad_",
+            "dynamicFacing", "mirrorSprite", "killOnContact", "startAttackingImmediately"
+        }
+    end
+end
 
 bossMasterController.placements = {
-    name = "Boss Controller",
-    data = {
-        hitboxMetadataPath = "",
-        attacksPath = "",
-        eventsPath = "",
-        functionsPath = "",
-        patternsPath = "",
-        bossID = "",
-        bossSprite = "",
-        bossHealthMax = 3,
-        hurtMode = "playerContact",
-        bossHitCooldown = "0.5",
-        maxFall = "90",
-        baseGravityMultiplier = "1.0",
-        sidekickCooldown = "5.0",
-        dynamicFacing = true,
-        mirrorSprite = false,
-        killOnContact = false,
-        startAttackingImmediately = false,
-        sidekickFreeze = false
+    {
+        name = "Boss Controller",
+        data = {
+            hitboxMetadataPath = "",
+            attacksPath = "",
+            eventsPath = "",
+            functionsPath = "",
+            patternsPath = "",
+            bossID = "",
+            bossSprite = "",
+            bossHealthMax = 3,
+            hurtMode = "playerContact",
+            bossHitCooldown = "0.5",
+            maxFall = "90",
+            baseGravityMultiplier = "1.0",
+            dynamicFacing = true,
+            mirrorSprite = false,
+            killOnContact = false,
+            startAttackingImmediately = false
+        }
+    },
+    {
+        name = "Boss Controller (Sidekick Mode)",
+        data = {
+            hitboxMetadataPath = "",
+            attacksPath = "",
+            eventsPath = "",
+            functionsPath = "",
+            patternsPath = "",
+            bossID = "",
+            bossSprite = "",
+            bossHealthMax = 3,
+            hurtMode = "sidekickAttack",
+            bossHitCooldown = "0.5",
+            maxFall = "90",
+            baseGravityMultiplier = "1.0",
+            sidekickCooldown = "5.0",
+            dynamicFacing = true,
+            mirrorSprite = false,
+            killOnContact = false,
+            startAttackingImmediately = false,
+            sidekickFreeze = false
+        }
     }
 }
-
---[[function bossMasterController.texture(room, entity)
-    local texture = string.format("characters/%s00", entity.bossSprite or "")
-
-    return (entity.bossSprite == "") and texture or "loenn/BossesHelper/BossController"
-end]]--
 
 return bossMasterController
