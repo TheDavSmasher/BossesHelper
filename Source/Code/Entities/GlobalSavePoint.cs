9b1d0517bd84857bfec172b2d49232ea5e67f8d3
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-01-06 11:47:52 -0700

    Added fields for custom talker values

--- Source/Code/Entities/GlobalSavePoint.cs
+++ Source/Code/Entities/GlobalSavePoint.cs
@@ -23,6 +23,10 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
 
         private TalkComponent talker;
 
+        private readonly Rectangle talkerRect;
+
+        private readonly Vector2 talkerOffset;
+
         private readonly string filepath;
 
         private LuaFunction onInteract;
@@ -42,16 +46,21 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
         public GlobalSavePoint(EntityData entityData, Vector2 offset)
             : base(entityData.Position + offset)
         {
-            Add(Changer = new(entityData.Level, entityData.Nodes.First(), entityData.Enum("respawnType", Player.IntroTypes.Respawn)));
+            Add(Changer = new(entityData.Level, entityData.Nodes.FirstOrDefault(Position),
+                entityData.Enum("respawnType", Player.IntroTypes.Respawn)));
             filepath = entityData.String("luaFile");
             string spriteName = entityData.String("savePointSprite");
             GFX.SpriteBank.TryCreate(spriteName, out Sprite);
+            Add(Sprite);
+            talkerRect = new Rectangle(entityData.Int("rectXOffset"),
+                entityData.Int("rectYOffset"), entityData.Int("rectWidth"), 8);
+            talkerOffset = new Vector2(entityData.Float("talkerXOffset"), entityData.Float("talkerYOffset"));
         }
 
         public override void Awake(Scene scene)
         {
             base.Awake(scene);
-            talker = new TalkComponent(new Rectangle(0, 0, Sprite.width, 8), Changer.spawnPoint - Position, OnTalk)
+            talker = new TalkComponent(talkerRect, talkerOffset, OnTalk)
             {
                 Enabled = true,
                 PlayerMustBeFacing = false
