c3dd1a1c3c057df7fa31b0155bfd9c5db58c673c
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-01-06 11:47:29 -0700

    Added fields with tooltips

--- Loenn/entities/globalSavePoint.lua
+++ Loenn/entities/globalSavePoint.lua
@@ -17,7 +17,7 @@ local respawnOpts = {
 globalSavePoint.name = "BossesHelper/PlayerSavePoint"
 globalSavePoint.depth = 0
 --globalSavePoint.texture = ""
-globalSavePoint.nodeLimits = {1, 1}
+globalSavePoint.nodeLimits = {0, 1}
 globalSavePoint.nodeLineRenderType = "line"
 globalSavePoint.fieldInformation = {
     respawnType = {
@@ -28,12 +28,51 @@ globalSavePoint.fieldInformation = {
         fieldType = "path",
         allowedExtensions = {"lua"},
         allowMissingPath = false
+    },
+    rectWidth = {
+        validator = function (string)
+            res = tonumber(string)
+            return res == nil or res >= 0 and string:find("%.") == nil
+        end
+    },
+    rectXOffset = {
+        validator = function (string)
+            res = tonumber(string)
+            return res == nil or res >= 0 and string:find("%.") == nil
+        end
+    },
+    rectYOffset = {
+        validator = function (string)
+            res = tonumber(string)
+            return res == nil or res >= 0 and string:find("%.") == nil
+        end
+    },
+    talkerXOffset = {
+        fieldType = "number",
+        minimumValue = 0
+    },
+    talkerYOffset = {
+        fieldType = "number",
+        minimumValue = 0
     }
 }
---globalSavePoint.fieldOrder = {}
+
+globalSavePoint.ignoredFields = {
+    "_name", "_id", "originX", "originY", "height"
+}
+
+globalSavePoint.fieldOrder = {
+    "x", "y", "luaFile", "savePointSprite", "respawnType", "rectWidth", "rectXOffset", "rectYOffset", "talkerXOffset", "talkerYOffset"
+}
+
 globalSavePoint.placements = {
     name = "Player Save Point",
     data = {
+        rectWidth = "0",
+        rectXOffset = "0",
+        rectYOffset = "0",
+        talkerXOffset = "0.0",
+        talkerYOffset = "0.0",
         luaFile = "",
         savePointSprite = "",
         respawnType = "Respawn"
