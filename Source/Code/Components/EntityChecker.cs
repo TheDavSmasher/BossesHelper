f23290a0f4ea94f21bd5ab09e852742453fa1b11
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-03-21 12:06:53 -0600

    Added EntityChecker Component

---
+++ Source/Code/Components/EntityChecker.cs
@@ -0,0 +1,32 @@
+﻿using NLua;
+using Monocle;
+using System;
+using System.Linq;
+
+namespace Celeste.Mod.BossesHelper.Code.Components
+{
+    public class EntityChecker(Func<bool> checker, Action<Entity> action, bool stateNeeded, bool removeOnComplete = true)
+        : Component(active: true, visible: false)
+    {
+        public Func<bool> checker = checker;
+
+        public bool stateNeeded = stateNeeded;
+
+        public Action<Entity> action = action;
+
+        private readonly bool removeOnComplete = removeOnComplete;
+
+        public EntityChecker(LuaFunction checker, LuaFunction action, bool stateNeeded)
+            : this(() => (bool) checker.Call().FirstOrDefault(), (e) => action.Call(e), stateNeeded) { }
+
+        public override void Update()
+        {
+            if (checker() == stateNeeded)
+            {
+                action.Invoke(Entity);
+                if (removeOnComplete)
+                    RemoveSelf();
+            }
+        }
+    }
+}
