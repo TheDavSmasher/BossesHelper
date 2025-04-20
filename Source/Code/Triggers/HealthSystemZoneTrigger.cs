5e5c3dc2c9a1624d7275a3305564344169bf3128
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-04-20 16:32:53 -0600

    Created HealthSystemZone trigger that will enable and disable the existing health system while inside or outside the trigger

---
+++ Source/Code/Triggers/HealthSystemZoneTrigger.cs
@@ -0,0 +1,38 @@
+﻿using Celeste.Mod.BossesHelper.Code.Entities;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.BossesHelper.Code.Triggers
+{
+    [CustomEntity("BossesHelper/HealthSystemZoneTrigger")]
+    public class HealthSystemZoneTrigger(EntityData data, Vector2 offset) : Trigger(data, offset)
+    {
+        private readonly bool enableState = data.Bool("enableState");
+
+        public override void OnEnter(Player player)
+        {
+            base.OnEnter(player);
+            if (enableState)
+            {
+                SceneAs<Level>().Tracker.GetEntity<HealthSystemManager>()?.EnableHealthSystem();
+            }
+            else
+            {
+                SceneAs<Level>().Tracker.GetEntity<HealthSystemManager>()?.DisableHealthSystem();
+            }
+        }
+
+        public override void OnLeave(Player player)
+        {
+            base.OnLeave(player);
+            if (enableState)
+            {
+                SceneAs<Level>().Tracker.GetEntity<HealthSystemManager>()?.DisableHealthSystem();
+            }
+            else
+            {
+                SceneAs<Level>().Tracker.GetEntity<HealthSystemManager>()?.EnableHealthSystem();
+            }
+        }
+    }
+}
