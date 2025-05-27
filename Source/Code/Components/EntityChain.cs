60f67efb859a19b53354b4dea04d40685c4d37d1
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-27 15:46:19 -0600

    Reformatted fields

--- Source/Code/Components/EntityChain.cs
+++ Source/Code/Components/EntityChain.cs
@@ -3,27 +3,26 @@ using Microsoft.Xna.Framework;
 
 namespace Celeste.Mod.BossesHelper.Code.Components
 {
-    public class EntityChain(Entity entity, bool chainPosition, bool removeTogether = false) : Component(active: true, visible: false)
+    public class EntityChain(Entity chained, bool chainPosition, bool removeTogether = false)
+        : Component(active: true, visible: false)
     {
-        public Entity chained = entity;
+        public Entity Chained => chained;
 
         public bool chainedPosition = chainPosition;
 
         public Vector2 positionOffset;
 
-        private readonly bool removeTogether = removeTogether;
-
         public override void Added(Entity entity)
         {
             base.Added(entity);
-            positionOffset = chained.Position - Entity.Position;
+            positionOffset = Chained.Position - Entity.Position;
         }
 
         public override void Update()
         {
             if (chainedPosition)
             {
-                chained.Position = Entity.Position + positionOffset;
+                Chained.Position = Entity.Position + positionOffset;
             }
         }
 
@@ -31,7 +30,7 @@ namespace Celeste.Mod.BossesHelper.Code.Components
         {
             base.Removed(entity);
             if (removeTogether)
-                chained.RemoveSelf();
+                Chained.RemoveSelf();
         }
     }
 }
