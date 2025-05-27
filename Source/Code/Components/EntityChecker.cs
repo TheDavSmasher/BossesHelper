0517af95b9f423855ec23bdff9c368946949685d
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-27 15:29:42 -0600

    Removed unnecessary class field

--- Source/Code/Components/EntityChecker.cs
+++ Source/Code/Components/EntityChecker.cs
@@ -8,8 +8,6 @@ namespace Celeste.Mod.BossesHelper.Code.Components
     public class EntityChecker(Func<bool> checker, Action<Entity> action, bool stateNeeded = true, bool removeOnComplete = true)
         : StateChecker(action, stateNeeded, removeOnComplete)
     {
-        private readonly Func<bool> checker = checker;
-
         public EntityChecker(LuaFunction checker, LuaFunction action, bool stateNeeded = true, bool removeOnComplete = true)
             : this(() => (bool) checker.Call().FirstOrDefault(), (e) => action.Call(e), stateNeeded, removeOnComplete) { }
 
