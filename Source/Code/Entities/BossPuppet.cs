ca2dc16e2ba66599174d9db13042fdacb7f9f722
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-28 12:45:18 -0600

    Removed unused import

--- Source/Code/Entities/BossPuppet.cs
+++ Source/Code/Entities/BossPuppet.cs
@@ -4,7 +4,6 @@ using Celeste.Mod.BossesHelper.Code.Other;
 using Microsoft.Xna.Framework;
 using Monocle;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
