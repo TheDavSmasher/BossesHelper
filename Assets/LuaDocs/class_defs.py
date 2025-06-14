67cc06cc850b83bb43e5194a3a54c7ff9cb8b2d6
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-13 21:37:28 -0600

    Moved creating of signature in class_defs.py and removed signature field

--- Assets/LuaDocs/class_defs.py
+++ Assets/LuaDocs/class_defs.py
@@ -17,14 +17,33 @@ class FunctionParam(FunctionType):
 @dataclass
 class Function:
     name: str
-    signature: str
+    full_name: str
     description: str
     params: list[FunctionParam]
     returns: list[FunctionType]
 
-    @property
-    def full_name(self):
-        return self.name + ' ' + self.signature
+    def __init__(self, name: str, description: str, params: list[FunctionParam], returns: list[FunctionType]):
+        self.name = name
+        self.description = description
+        self.params = params
+        self.returns = returns
+
+        first = True
+        optional_params = 0
+        self.full_name = self.name + " ("
+        for param in self.params:
+            if param.optional:
+                optional_params += 1
+                self.full_name += '['
+            if not first:
+                self.full_name += ", "
+            self.full_name += param.name
+            if param.optional and len(param.default) > 0:
+                self.full_name += '=' + param.default
+            first = False
+        for _ in range(optional_params):
+            self.full_name += ']'
+        self.full_name += ')'
 
 
 @dataclass
