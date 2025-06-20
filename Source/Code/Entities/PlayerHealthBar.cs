83ff27bf50587ca026a77b02e751d0eed842e551
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-20 16:56:44 -0600

    Made PlayerHealthBar extend HealthIconList instead of only having one

--- Source/Code/Entities/PlayerHealthBar.cs
+++ Source/Code/Entities/PlayerHealthBar.cs
@@ -4,34 +4,13 @@ using static Celeste.Mod.BossesHelper.Code.Helpers.BossesHelperUtils;
 namespace Celeste.Mod.BossesHelper.Code.Entities
 {
     [Tracked(false)]
-    public class PlayerHealthBar : HudEntity
+    public class PlayerHealthBar : HealthIconList
     {
         private static BossesHelperSession.HealthSystemData HealthData => BossesHelperModule.Session.healthData;
 
-        public new bool Visible
-        {
-            get
-            {
-                return healthIcons.Visible;
-            }
-            set
-            {
-                healthIcons.Visible = value;
-            }
-        }
-
-        private readonly HealthIconList healthIcons;
-
         internal PlayerHealthBar() : base(HealthData.globalController)
         {
-            healthIcons = new(IsGlobal);
             Visible = HealthData.startVisible;
-        }
-
-        public override void Added(Scene scene)
-        {
-            base.Added(scene);
-            scene.Add(healthIcons);
         }
 
         public override void Awake(Scene scene)
@@ -41,30 +20,8 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
                 RemoveSelf();
                 return;
             }
-            healthIcons.Clear();
-            healthIcons.RefillHealth();
+            Clear();
             base.Awake(scene);
-        }
-
-        public override void Removed(Scene scene)
-        {
-            base.Removed(scene);
-            healthIcons.RemoveSelf();
-        }
-
-        public void RefillHealth(int? upTo = null)
-        {
-            healthIcons.RefillHealth(upTo);
-        }
-
-        public void IncreaseHealth(int amount = 1)
-        {
-            healthIcons.IncreaseHealth(amount);
-        }
-
-        public void DecreaseHealth(int amount = 1)
-        {
-            healthIcons.DecreaseHealth(amount);
         }
     }
 }
