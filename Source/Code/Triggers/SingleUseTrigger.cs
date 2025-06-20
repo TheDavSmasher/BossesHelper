58542a04ed019d7c0b6a9a462fc4d271c85a98ce
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-19 20:01:07 -0600

    Created SingleUseTrigger class

---
+++ Source/Code/Triggers/SingleUseTrigger.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.BossesHelper.Code.Triggers
+{
+    public abstract class SingleUseTrigger(EntityData data, Vector2 offset, EntityID id, bool permanent = false)
+            : Trigger(data, offset)
+    {
+        public override void Removed(Scene scene)
+        {
+            base.Removed(scene);
+            if (permanent)
+            {
+                SceneAs<Level>().Session.DoNotLoad.Add(id);
+            }
+        }
+    }
+}
