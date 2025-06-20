f8738e3e2d0a4234c09f84162fddf51fae5898e4
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-19 21:00:40 -0600

    Added ConsumeAfter method with new parameter

--- Source/Code/Triggers/SingleUseTrigger.cs
+++ Source/Code/Triggers/SingleUseTrigger.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.Xna.Framework;
 using Monocle;
+using System;
 
 namespace Celeste.Mod.BossesHelper.Code.Triggers
 {
-    public abstract class SingleUseTrigger(EntityData data, Vector2 offset, EntityID id, bool permanent = false)
+    public abstract class SingleUseTrigger(EntityData data, Vector2 offset, EntityID id, bool permanent = false, bool removeOnUse = false)
             : Trigger(data, offset)
     {
         protected readonly EntityData entityData = data;
 
         protected readonly EntityID ID = id;
+
+        protected void ConsumeAfter(Action change)
+        {
+            change();
+            if (removeOnUse)
+            {
+                RemoveSelf();
+            }
+        }
 
         public override void Removed(Scene scene)
         {
