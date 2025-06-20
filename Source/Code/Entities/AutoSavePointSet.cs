58decd486b95164a3bca7706c623ed91ce9d2bf9
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-19 19:03:14 -0600

    Changed to Primary Constructor

--- Source/Code/Entities/AutoSavePointSet.cs
+++ Source/Code/Entities/AutoSavePointSet.cs
@@ -6,42 +6,22 @@ using Monocle;
 namespace Celeste.Mod.BossesHelper.Code.Entities
 {
     [CustomEntity("BossesHelper/AutoSavePointSet")]
-    public class AutoSavePointSet : Entity
+    public class AutoSavePointSet(EntityData data, Vector2 pos, EntityID ID) : Entity(pos)
     {
-        private readonly EntityID ID;
+        private readonly Player.IntroTypes spawnType = data.Enum<Player.IntroTypes>("respawnType");
 
-        private readonly Player.IntroTypes spawnType;
+        private readonly Vector2? spawnPosition = data.FirstNodeNullable();
 
-        private readonly Vector2? spawnPosition;
-
-        private readonly bool onlyOnce;
+        private readonly bool onlyOnce = data.Bool("onlyOnce");
 
         private GlobalSavePointChanger Changer;
-
-        public AutoSavePointSet(EntityData data, Vector2 _, EntityID id)
-            : base()
-        {
-            ID = id;
-            spawnType = data.Enum<Player.IntroTypes>("respawnType");
-            if (data.FirstNodeNullable() is Vector2 spawn)
-            {
-                spawnPosition = spawn;
-            }
-            onlyOnce = data.Bool("onlyOnce");
-        }
 
         public override void Added(Scene scene)
         {
             base.Added(scene);
-            Session session = (scene as Level).Session;
-            if (spawnPosition is Vector2 node)
-            {
-                Add(Changer = new(ID, node, spawnType));
-            }
-            else if (session.RespawnPoint is Vector2 spawn)
-            {
-                Add(Changer = new(ID, spawn, spawnType));
-            }
+            Add(Changer = new(ID,
+                spawnPosition is Vector2 node ? node : SceneAs<Level>().Session.RespawnPoint is Vector2 spawn ? spawn : Position,
+                spawnType));
         }
 
         public override void Awake(Scene scene)
