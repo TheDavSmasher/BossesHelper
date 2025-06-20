edd0a0a6c45ffaf23e97dd85f0c57bc7119996e8
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-20 16:22:44 -0600

    Changed to actually use System Exception

--- Assets/LuaBossHelper/cutscene_helper.lua
+++ Assets/LuaBossHelper/cutscene_helper.lua
@@ -3,7 +3,7 @@
 local cutsceneHelper = {}
 
 local celesteMod = require("#celeste.mod")
-local systemException = require("#System.Exception")
+local systemException = require("#system.systemexception")
 
 local function threadProxyResume(self, ...)
     local thread = self.value
@@ -16,7 +16,7 @@ local function threadProxyResume(self, ...)
 
     -- The error message should be returned as an exception and not a string
     if not success then
-        return success, message
+        return success, systemException(message)
     end
 
     return success, message
