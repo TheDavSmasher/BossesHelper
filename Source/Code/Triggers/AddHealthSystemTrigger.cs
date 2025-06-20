c2e0224f0f8bf689a5a21a23d24508683362b878
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-20 16:38:05 -0600

    Changed logic to disable and renable health system instead of removing manager if existing

--- Source/Code/Triggers/AddHealthSystemTrigger.cs
+++ Source/Code/Triggers/AddHealthSystemTrigger.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Celeste.Mod.BossesHelper.Code.Entities;
+using Celeste.Mod.BossesHelper.Code.Helpers;
 using Celeste.Mod.Entities;
 using System.Collections;
 using Monocle;
@@ -8,7 +9,7 @@ namespace Celeste.Mod.BossesHelper.Code.Triggers
 {
     [CustomEntity("BossesHelper/AddHealthSystemTrigger")]
     public class AddHealthSystemTrigger(EntityData data, Vector2 offset, EntityID id)
-        : SingleUseTrigger(data, offset, id, data.Bool("onlyOnce", true), true)
+        : SingleUseTrigger(data, offset, id, data.Bool("onlyOnce", true))
     {
         public override void OnEnter(Player player)
         {
@@ -18,9 +19,18 @@ namespace Celeste.Mod.BossesHelper.Code.Triggers
 
         private IEnumerator RemoveAndReadd()
         {
-            Scene.Tracker.GetEntity<HealthSystemManager>()?.RemoveSelf();
-            yield return null;
-            ConsumeAfter(() => Scene.Add(new HealthSystemManager(entityData, Vector2.Zero)));
+            if (Scene.GetEntity<HealthSystemManager>() is HealthSystemManager manager)
+            {
+                manager.DisableHealthSystem(false);
+                manager.UpdateSessionData(entityData);
+                yield return null;
+                manager.EnableHealthSystem(false);
+            }
+            else
+            {
+                Scene.Add(new HealthSystemManager(entityData, Vector2.Zero));
+            }
+            RemoveSelf();
         }
     }
 }
