baf9303059a6453b6f40330a9b526872b271fb70
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-20 19:31:58 -0600

    Moved DamageController to separate file through partial class

--- Source/Code/Entities/HealthSystemManager.cs
+++ Source/Code/Entities/HealthSystemManager.cs
@@ -2,11 +2,6 @@
 using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
-using System;
-using NLua;
-using System.Collections;
-using System.Collections.Generic;
-using static Celeste.Mod.BossesHelper.Code.Helpers.LuaBossHelper;
 using static Celeste.Mod.BossesHelper.Code.Helpers.BossesHelperUtils;
 
 namespace Celeste.Mod.BossesHelper.Code.Entities
@@ -39,116 +34,6 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
                 Visible = HealthData.startVisible;
                 Clear();
                 base.Awake(scene);
-            }
-        }
-
-        [Tracked(false)]
-        private class DamageController() : GlobalEntity(false)
-        {
-            private LuaFunction onRecover;
-
-            private LuaFunction onDamage;
-
-            public void UpdateState(Player player, PlayerHealthBar healthBar)
-            {
-                ChangeGlobalState(HealthData.globalController);
-                LuaFunction[] array = LoadLuaFile(new Dictionary<object, object>
-                {
-                    { "player", player },
-                    { "healthBar", healthBar }
-                },
-                HealthData.onDamageFunction, "getFunctionData", 2);
-                onDamage = array[0];
-                onRecover = array[1];
-            }
-
-            public void TakeDamage(Vector2 direction, int amount = 1, bool silent = false, bool stagger = true, bool evenIfInvincible = false)
-            {
-                Level Level = SceneAs<Level>();
-                if (Level.InCutscene ||
-                    !evenIfInvincible && (ModSession.damageCooldown > 0 || SaveData.Instance.Assists.Invincible || amount <= 0) ||
-                    Engine.Scene.GetPlayer() is not Player entity || entity.StateMachine.State == Player.StCassetteFly)
-                {
-                    return;
-                }
-                ModSession.damageCooldown = HealthData.damageCooldown;
-                if ((ModSession.currentPlayerHealth -= amount) > 0)
-                {
-                    if (!silent)
-                    {
-                        Level.Shake();
-                        Input.Rumble(RumbleStrength.Strong, RumbleLength.Long);
-                        Level.Flash(Color.Red * 0.3f);
-                        Audio.Play("event:/char/madeline/predeath");
-                        if (HealthData.playerStagger && stagger)
-                            Add(new Coroutine(PlayerStagger(entity.Position, direction)));
-                        if (HealthData.playerBlink)
-                            Add(new Coroutine(PlayerInvincible()));
-                        if (onDamage != null)
-                            Add(new Coroutine(onDamage.ToIEnumerator()));
-                    }
-                }
-                else
-                {
-                    entity.Die(direction);
-                }
-            }
-
-            public void RecoverHealth(int amount = 1)
-            {
-                ModSession.currentPlayerHealth += amount;
-                if (onRecover != null)
-                    Add(new Coroutine(onRecover.ToIEnumerator()));
-            }
-
-            public void RefillHealth()
-            {
-                ModSession.currentPlayerHealth = HealthData.playerHealthVal;
-                if (onRecover != null)
-                    Add(new Coroutine(onRecover.ToIEnumerator()));
-            }
-
-            private IEnumerator PlayerStagger(Vector2 from, Vector2 bounce)
-            {
-                if (bounce != Vector2.Zero)
-                {
-                    Celeste.Freeze(0.05f);
-                    yield return null;
-                    Vector2 to = new(from.X + (!(bounce.X < 0f) ? 1 : -1) * 20f, from.Y - 5f);
-                    Tween tween = Tween.Set(this, Tween.TweenMode.Oneshot, 0.2f, Ease.CubeOut, delegate (Tween t)
-                    {
-                        Vector2 val = from + (to - from) * t.Eased;
-                        if (Engine.Scene.GetPlayer() is Player player)
-                        {
-                            player.MoveToX(val.X);
-                            player.MoveToY(val.Y);
-                            player.Sprite.Rotation = (float)(Math.Floor(t.Eased * 4f) * 6.2831854820251465);
-                        }
-                    });
-                    yield return tween.Wait();
-                }
-            }
-
-            private IEnumerator PlayerInvincible()
-            {
-                static void ChangeVisible(bool state)
-                {
-                    if (Engine.Scene.GetPlayer() is Player player)
-                    {
-                        player.Sprite.Visible = state;
-                        player.Hair.Visible = state;
-                    }
-                }
-                int times = 1;
-                Tween tween = Tween.Set(this, Tween.TweenMode.Oneshot, HealthData.damageCooldown, Ease.CubeOut, delegate
-                {
-                    if (Scene.OnInterval(0.02f))
-                    {
-                        ChangeVisible(times++ % 3 == 0);
-                    }
-                });
-                yield return tween.Wait();
-                ChangeVisible(true);
             }
         }
 
