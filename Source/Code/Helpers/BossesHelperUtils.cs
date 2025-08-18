cf2e5ffc6408b803142b94b6cff39991fe203ec1
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-08-18 02:26:55 -0600

    Created Stopwatch component

--- Source/Code/Helpers/BossesHelperUtils.cs
+++ Source/Code/Helpers/BossesHelperUtils.cs
@@ -225,6 +225,24 @@ namespace Celeste.Mod.BossesHelper.Code.Helpers
 			}
 		}
 
+		public class Stopwatch(float fullTime) : Component(true, false)
+		{
+			public float TimeLeft { get; set; }
+
+			public bool Finished => TimeLeft <= 0;
+
+			public void Reset()
+			{
+				TimeLeft = fullTime;
+			}
+
+			public override void Update()
+			{
+				if (TimeLeft > 0)
+					TimeLeft -= Engine.DeltaTime;
+			}
+		}
+
 		public interface IMonocleCollection<T> : IReadOnlyCollection<T>
 		{
 			public bool Active
