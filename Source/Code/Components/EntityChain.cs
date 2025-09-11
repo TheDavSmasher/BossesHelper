4a8b07a22814542d2f33587fc72242836d00761c
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-09-11 15:40:44 -0600

    Updated EntityChain API to use Active status instead of extra boolean

--- Source/Code/Components/EntityChain.cs
+++ Source/Code/Components/EntityChain.cs
@@ -3,25 +3,20 @@ using Monocle;
 
 namespace Celeste.Mod.BossesHelper.Code.Components
 {
-	public class EntityChain(Entity chained, bool chainPosition, bool removeTogether = false)
-		: Component(active: true, visible: false)
+	public class EntityChain(Entity chained, bool startChained, bool removeTogether = false)
+		: Component(startChained, visible: false)
 	{
-		public bool chainedPosition = chainPosition;
-
-		public Vector2 positionOffset;
+		public Vector2 ChainOffset;
 
 		public override void Added(Entity entity)
 		{
 			base.Added(entity);
-			positionOffset = chained.Position - Entity.Position;
+			ChainOffset = chained.Position - Entity.Position;
 		}
 
 		public override void Update()
 		{
-			if (chainedPosition)
-			{
-				chained.Position = Entity.Position + positionOffset;
-			}
+			chained.Position = Entity.Position + ChainOffset;
 		}
 
 		public override void Removed(Entity entity)
