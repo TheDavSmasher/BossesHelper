719b6003b25ea90bdbba5163fea5102a3e3db3b8
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-09-19 18:00:39 -0600

    Removed onCollide field in favor of lambda method

--- Source/Code/Entities/AttackActor.cs
+++ Source/Code/Entities/AttackActor.cs
@@ -6,20 +6,12 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
 {
 	public class AttackActor : BossActor
 	{
-		private readonly LuaFunction onCollide;
-
 		public AttackActor(Vector2 position, Collider attackbox, LuaFunction onPlayer, bool startCollidable,
 			bool solidCollidable, string spriteName, float gravMult, float maxFall, float xScale = 1f, float yScale = 1f)
 			: base(position, spriteName, new Vector2(xScale, yScale), maxFall, startCollidable, solidCollidable, gravMult)
 		{
 			Collider = attackbox;
-			onCollide = onPlayer;
-			Add(new PlayerCollider(OnPlayer));
-		}
-
-		private void OnPlayer(Player player)
-		{
-			onCollide.Call(this, player);
+			Add(new PlayerCollider(player => onPlayer.Call(this, player)));
 		}
 
 		public void SetSolidCollisionActive(bool active)
--- Source/Code/Entities/AttackEntity.cs
+++ Source/Code/Entities/AttackEntity.cs
@@ -6,19 +6,11 @@ namespace Celeste.Mod.BossesHelper.Code.Entities
 {
 	public class AttackEntity : BossEntity
 	{
-		private readonly LuaFunction onCollide;
-
 		public AttackEntity(Vector2 position, Collider attackbox, LuaFunction onPlayer, bool startCollidable, string spriteName, float xScale = 1f, float yScale = 1f)
 			: base(position, spriteName, new Vector2(xScale, yScale), startCollidable)
 		{
 			Collider = attackbox;
-			onCollide = onPlayer;
-			Add(new PlayerCollider(OnPlayer));
-		}
-
-		private void OnPlayer(Player player)
-		{
-			onCollide.Call(this, player);
+			Add(new PlayerCollider(player => onPlayer.Call(this, player)));
 		}
 
 		public void SetCollisionActive(bool active)
