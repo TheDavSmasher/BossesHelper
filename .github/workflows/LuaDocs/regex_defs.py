d5b9f26803d065b0130ac6e15f0a4673ded9ddfa
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-09-23 18:44:27 -0600

    Modified regex for locals

--- .github/workflows/LuaDocs/regex_defs.py
+++ .github/workflows/LuaDocs/regex_defs.py
@@ -1,6 +1,6 @@
 import re
 
-LOCAL_R = r'^(local\s+\w+'
+LOCAL_R = r'^local\s+'
 ANN_R = r'---\s*@'
 TABLE = 'helpers'
 
@@ -10,10 +10,10 @@ FIELD_R = r'([\w.]+)\s*'
 REGION_P = re.compile(r'^--#region\s+(.*)')
 REGION_END_P = re.compile(r'^--#endregion')
 MODULE_P = re.compile(fr'{ANN_R}module\s+"[\w.]+"')
-MODULE_F_P = re.compile(fr'{LOCAL_R})\s+=\s+require')
+MODULE_F_P = re.compile(fr'({LOCAL_R}\w+)\s+=\s+require')
 FIELD_P = re.compile(fr'^{FIELD_R}=')
 FUNC_P = re.compile(fr'^function\s+{FIELD_R}\(([^)]*)\)')
-LOCAL_FUNC_P = re.compile(r'^local\s+function')
+LOCAL_FUNC_P = re.compile(fr'{LOCAL_R}function')
 FUNC_END_P = re.compile(r'^end$')
 
 TYPE_R = r'([\w?.|]+(?:<[^<>]+>)?(?:\([^)]*\))?(?:\[\])*)'
