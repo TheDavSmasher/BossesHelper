8995cc42dceb8ea9f06cedace37b283688833f04
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-10-20 12:37:52 -0600

    Added tag group add methods

--- Source/Code/Helpers/HitboxMetadata.cs
+++ Source/Code/Helpers/HitboxMetadata.cs
@@ -1,6 +1,5 @@
 ﻿using Monocle;
 using System.Collections.Generic;
-using System.Xml.Linq;
 using static Celeste.Mod.BossesHelper.Code.Entities.BossPuppet;
 using static Celeste.Mod.BossesHelper.Code.Helpers.BossesHelperUtils;
 
@@ -10,8 +9,15 @@ namespace Celeste.Mod.BossesHelper.Code.Helpers
 	{
 		public readonly EnumDict<ColliderOption, Dictionary<string, Collider>> ColliderOptions = new(_ => []);
 
+		private readonly Dictionary<string, EnumDict<ColliderOption, Collider>> TagGroups = [];
+
+		private readonly HashSet<string> TagGroupKeepOld = [];
+
 		public Dictionary<string, Collider> this[ColliderOption opt]
 			=> ColliderOptions[opt];
+
+		public EnumDict<ColliderOption, Collider> this[string tag]
+			=> TagGroups[tag];
 
 		public void Add(ColliderOption option, string tag, Collider collider)
 		{
@@ -21,6 +27,18 @@ namespace Celeste.Mod.BossesHelper.Code.Helpers
 				list.Add(collider);
 			else
 				this[option][tag] = new ColliderList(this[option][tag], collider);
+		}
+
+		public void Add(string tag, ColliderOption option, Collider collider)
+		{
+			if (collider == null ||
+				TagGroups.TryAdd(tag, new(opt => opt == option ? collider : null)) ||
+				this[tag].TryAdd(option, collider))
+				return;
+			if (this[tag][option] is ColliderList list)
+				list.Add(collider);
+			else
+				this[tag][option] = new ColliderList(this[tag][option], collider);
 		}
 	}
 }
