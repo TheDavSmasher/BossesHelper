1079bd6da4f9a3f0d93bf44c9952339778cb805a
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-10-30 14:32:53 -0600

    Extracted SingleUse to own file

--- Source/Code/Helpers/BossesHelperUtils.cs
+++ Source/Code/Helpers/BossesHelperUtils.cs
@@ -233,29 +233,6 @@ namespace Celeste.Mod.BossesHelper.Code.Helpers
 			}
 		}
 
-		public struct SingleUse<T> where T : struct
-		{
-			public SingleUse(T? val = null)
-			{
-				Value = val;
-			}
-
-			public T? Value
-			{
-				get
-				{
-					T? value = field;
-					field = null;
-					return value;
-				}
-				set;
-			}
-
-			public static implicit operator SingleUse<T>(T? v) => new(v);
-
-			public static implicit operator T?(SingleUse<T> s) => s.Value;
-		}
-
 		public class NullRange(uint? min, uint? max, uint? @default, Random random, int randomChance = 50)
 		{
 			private readonly uint? MinRange = min ?? max ?? @default;
---
+++ Source/Code/Helpers/SingleUse.cs
@@ -0,0 +1,25 @@
+﻿namespace Celeste.Mod.BossesHelper.Code.Helpers
+{
+	public struct SingleUse<T> where T : struct
+	{
+		public SingleUse(T? val = null)
+		{
+			Value = val;
+		}
+
+		public T? Value
+		{
+			get
+			{
+				T? value = field;
+				field = null;
+				return value;
+			}
+			set;
+		}
+
+		public static implicit operator SingleUse<T>(T? v) => new(v);
+
+		public static implicit operator T?(SingleUse<T> s) => s.Value;
+	}
+}
